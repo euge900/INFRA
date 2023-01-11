650ed9abbda3eb35faee8adc5f87d03e10a08b57
Eugenio Feliz <eugeniof13@gmail.com>
2023-01-11 11:18:58 -0500

    Added new file

---
+++ FindADGroupandFormatCorrectly.ps1
@@ -0,0 +1,1 @@
+Get-ADGroup -Filter * -Properties description | ? {$_.name -like "*braincube*"} | select name, description | Export-Csv -Path C:\Temp\BraincubeGroups.csv -NoTypeInformation | Format-Table -Wrap
